5c2ff58baf088bd8f3d6cafb371e7f855fc46550
Daniel-Oshione <omobsdaniel@gmail.com>
2024-10-25 17:51:06 +0100

    learnt storing in array

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './interface/user.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('home')
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get('home')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  // @Post('products')
+  // getProducts(){
+  //   return this.appService.addProducts();
+  // }
+  @Post('user')
+  async createUser(@Body() payload){
+   await this.appService.create(payload);
   }
 
-  @Post('products')
-  getProducts(){
-    return this.appService.addProducts();
+  @Get()
+  async findAllUsers(): Promise<User[]>{
+    return await this.appService.findAll();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,49 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interface/user.interface';
 import { STATUS_CODES } from 'http';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return `we are, in year ${new Date().getFullYear()} and month is ${new Date().getMonth()}th. The date is also ${new Date().getDate()}.`;
+  private readonly users: User[] = []
+ 
+  create(user: User){
+    this.users.push(user);
   }
 
-  //to make posts
-  async addProducts(){
-     const productDetails = {
-      item1: {
-        id: 1,
-        name: 'Shoe',
-        price: '$50',
-        color: 'black',
-        isAvailable: 'true'
-      },
-      item2: {
-        id: 2,
-        name: 'Shoe',
-        price: '$150',
-        color: 'White',
-        isAvailable: 'true'
-      },
-      item3: {
-        id: 3,
-        name: 'Shoe',
-        price: '$60',
-        color: 'gray',
-        isAvailable: 'false'
-      },
-      item4: {
-        id: 4,
-        name: 'Shoe',
-        price: '$20',
-        color: 'orange',
-        isAvailable: 'true'
-      }
-     }
-     
-     return {
-      statusCode: 201,
-      message: 'Products added!',
-      goods: productDetails
-     }
+  findAll():User[]{
+    return this.users;
   }
-}
+  //to make posts
+  // async addProducts(){
+  //    const productDetails = {
+  //     item1: {
+  //       id: 1,
+  //       name: 'Shoe',
+  //       price: '$50',
+  //       color: 'black',
+  //       isAvailable: 'true'
+  //     },
+  //     item2: {
+  //       id: 2,
+  //       name: 'Shoe',
+  //       price: '$150',
+  //       color: 'White',
+  //       isAvailable: 'true'
+  //     },
+  //     item3: {
+  //       id: 3,
+  //       name: 'Shoe',
+  //       price: '$60',
+  //       color: 'gray',
+  //       isAvailable: 'false'
+  //     },
+  //     item4: {
+  //       id: 4,
+  //       name: 'Shoe',
+  //       price: '$20',
+  //       color: 'orange',
+  //       isAvailable: 'true'
+  //     }
+  //    }
+     
+  //    return {
+  //     statusCode: 201,
+  //     message: 'Products added!',
+  //     goods: productDetails
+  //    };
+  // };
+};
